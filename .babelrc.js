8858065319101b65224a92c199da69a01781fbdf
zhouwei <371311231@qq.com>
2021-09-16 13:50:09 +0800

    :zap: chore: 添加jest支持

--- .babelrc.js
+++ .babelrc.js
@@ -7,7 +7,8 @@ module.exports = {
         "useBuiltIns": false, // 是否开启自动支持 polyfill
         "modules": false, // 模块使用 es modules ，不使用 commonJS 规范
         // "targets": "> 0.25%, last 2 versions, iOS >= 8, Android >= 4.4, not dead"
-      }
+      },
+      "@babel/preset-typescript"
     ]
   ],
   plugins: [
--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,15 +1,15 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { babel } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { babel } from '@rollup/plugin-babel'
 import serve from 'rollup-plugin-serve'
-import pkg from '../package.json';
+import pkg from '../package.json'
 
-const path = require('path');
+const path = require('path')
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== 'production'
 
-console.log('----------dev:', process.env.NODE_ENV, isDev);
+console.log('----------dev:', process.env.NODE_ENV, isDev)
 
 // 一段自定义的内容，以下内容会添加到打包结果中
 const footer = `
@@ -30,7 +30,7 @@ export default {
       footer,
       globals: {
         lodash: '_'
-      },
+      }
     },
     {
       file: pkg.module,
@@ -38,7 +38,7 @@ export default {
       footer,
       globals: {
         lodash: '_'
-      },
+      }
     },
     {
       file: pkg.browser,
@@ -48,8 +48,8 @@ export default {
       sourcemap: true,
       globals: {
         lodash: '_'
-      },
-    },
+      }
+    }
   ],
   external: ['lodash'], // 与output.global同步使用
   plugins: [
@@ -58,17 +58,19 @@ export default {
     commonjs(),
     babel({
       exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
+      babelHelpers: 'runtime'
     }),
-    isDev ? serve({
-      open: false,
-      host: 'localhost',
-      port: 8888,
-      contentBase: [resolveFile('example'), resolveFile('lib')],
-      historyApiFallback: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    }) : null
-  ],
+    isDev
+      ? serve({
+        open: false,
+        host: 'localhost',
+        port: 8888,
+        contentBase: [resolveFile('example'), resolveFile('lib')],
+        historyApiFallback: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+      : null
+  ]
 }
