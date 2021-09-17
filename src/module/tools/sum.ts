5312c0184fb9270056d984d0d604f9ce524cfe43
zhouwei <371311231@qq.com>
2021-09-17 12:46:38 +0800

    :green_heart: ci: 调整lint-staged

--- src/module/tools/sum.ts
+++ src/module/tools/sum.ts
@@ -2,11 +2,4 @@ const sum = (x: number, y: number) => {
   console.log('this is fn sum:', x + y);
 };
 
-const a = 1;
-const b = 1;
-
-if (false) {
-  console.log('false');
-}
-
 export default sum;
